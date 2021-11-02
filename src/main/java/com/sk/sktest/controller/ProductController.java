6c286b85ebb17df223520bf4c75f3f6625a64918
Sam Kuang <samkuang@126.com>
2021-11-02 15:03:10 +0800

    merge gitignore

---
+++ src/main/java/com/sk/sktest/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.sk.sktest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping("/detail")
+    public String productDetail(){
+        return "Sam Kuang";
+    }
+}
---
+++ src/test/java/com/sk/sktest/SkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.sktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
