68508179c857f646948615adefe11b0659e40c45
Sam Kuang <samkuang@126.com>
2021-11-03 15:17:14 +0800

    exclude data source auto config

--- src/main/java/com/sk/sktest/SkTestApplication.java
+++ src/main/java/com/sk/sktest/SkTestApplication.java
@@ -3,7 +3,7 @@ package com.sk.sktest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class SkTestApplication {
 
     public static void main(String[] args) {
